eb1d789f38e79c5d9637c03fbab2d64462062d6c
Warda Jibril <Jibril.Warda@gmail.com>
2024-06-09 12:30:49 +0100

    current working file

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,8 @@
 from data_utils import DatabaseConnector
 import pandas as pd
+import tabula
+
+
 
 class DataExtractor():
     
@@ -11,22 +14,31 @@ class DataExtractor():
         # Read data from the specified table into a pandas DataFrame
         with engine.connect() as connection:
             df = pd.read_sql_table(table_name, connection)
-        
-        return print(f"TABLE NAME:{table_name} \n {df}")
+
+        print(f"TABLE NAME:{table_name} \n {df}")
+
+        return df
+    
+
+    
+    def retreive_pdf_data(self,link):
+
+        link = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+
+        pdf_data = tabula.read_pdf(link, pages="all")
+
+        return pdf_data
+
+
 
 db_connect = DatabaseConnector()
-
 creds = db_connect.read_db_creds()
-
 engine = db_connect.init_db_engine(creds)
-
 # Get the name of the table containing user data using list_db_tables method
 table_names = db_connect.list_db_names(engine)
-
 data_extract = DataExtractor()
+# Extract the table containing user data and return a pandas DataFrame
+user_data_df = data_extract.read_rds_table(db_connect,"legacy_users")
 
-# iterate through the list of table names to extract the table data 
-for table_name in table_names:
-    
-        # Extract the table containing user data and return a pandas DataFrame
-    user_data_df = data_extract.read_rds_table(db_connect, table_name)
+
+
