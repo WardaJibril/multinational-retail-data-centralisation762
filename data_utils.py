e3f6682cef43a6d4b23bd3349aa6c8bfdc830cc9
Warda Jibril <Jibril.Warda@gmail.com>
2024-06-09 12:30:13 +0100

    current working file

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,7 @@
 import yaml
 from sqlalchemy import create_engine, inspect
-
+from data_cleaning import DataCleaning
+from data_extraction import user_data_df
 class DatabaseConnector():
     def read_db_creds(self):
         #reads db credentails from yaml file
@@ -12,7 +13,7 @@ class DatabaseConnector():
         
         creds = self.read_db_creds()
         #creates sqlachemy url
-        db_url = f"{creds["type"]}://{creds["user"]}:{creds["password"]}@{creds["host"]}:{creds["port"]}/{creds["databse"]}"
+        db_url = f'postgresql+psycopg2://{creds["RDS_USER"]}:{creds["RDS_PASSWORD"]}@{creds["RDS_HOST"]}:{creds["RDS_PORT"]}/{creds["RDS_DATABASE"]}'
         #creates engine
         engine = create_engine(db_url)
         return engine
@@ -20,8 +21,29 @@ class DatabaseConnector():
     def list_db_names(self,engine):
         #gets a list of all tables in the database
         inspector = inspect(engine)
-        inspector.get_table_names()
-
+        table_names = inspector.get_table_names()
+        return table_names
+    
+    def upload_to_db(self, engine,df,table_name):
+         # needs to uplaod any df into speciifed table 
         
-        
 
+         with engine.connect() as connection:
+        
+            df.to_sql(table_name, con=connection, if_exists='append', index=False)
+
+         
+
+db_connect = DatabaseConnector()
+creds = db_connect.read_db_creds()
+engine = db_connect.init_db_engine(creds)
+
+table_names = db_connect.list_db_names(engine)
+
+print(table_names)
+
+clean = DataCleaning()
+cleaned_user_data = clean.clean_user_data(user_data_df)
+
+upload = DatabaseConnector()
+user_data_upload = upload.upload_to_db(engine, cleaned_user_data, "dim_users")
